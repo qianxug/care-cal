bf92a9a0be0e9c9d47f1657ceea793024c88b3fd
qianxug <qianxug@gmail.com>
2023-05-06 17:24:35 -0400

    still working on products page, product display structure is practically done

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,16 +1,74 @@
 import { React, ReactDOM, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Button, Breadcrumb, Card, Layout, Menu, Typography } from 'antd';
+import { Button, Form, Card, Layout, Menu, Input, List, Typography, Tag } from 'antd';
 import TopNavBar from './TopNavBar';
 
 const PRODUCTS_KEY = 'care-cal.products';
 
 function ProductDisplay({ product }) {
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  
+  function displayDaysOfWeek() {
+    if (product.routine.length === 0) {
+      return days.map((item) => <Tag>{item}</Tag>);
+    }  
+
+    return days.map((item) => {
+      const hasAm = product.routine.some((occurrence) => occurrence.meridian === 'am' && occurrence.dayOfWeek === item)
+      const hasPm = product.routine.some((occurrence) => occurrence.meridian === 'pm' && occurrence.dayOfWeek === item)
+
+      if (hasAm && hasPm)
+        return <Tag style={{ background: 'linear-gradient(to right, pink 50%, cyan 50%)'}}>{item}</Tag>
+      
+      else if (hasAm)
+        return <Tag style={{ background: 'pink'}}>{item}</Tag>
+
+      else if (hasPm)
+        return <Tag style={{ background: 'cyan'}}>{item}</Tag>
+      
+      else
+        return <Tag>{item}</Tag>
+    })
+
+    // for (var i = 0; i < days.length; i++) {
+    //   const hasAm = product.routine.some((item) => item.meridian === 'am' && item.dayOfWeek === days[i])
+    //   const hasPm = product.routine.some((item) => item.meridian === 'pm' && item.dayOfWeek === days[i])
+      
+
+    // }
+  }
+
   return (
-    <Card>
+    <Card style={{
+      backgroundColor: "lightgray"
+    }}>
       {product && (
         <Layout>
-          <p>Amogus with {product.label}</p>
+          <div style={{
+            backgroundColor: "lightgray",
+            display: "flex",
+            justifyContent: "flex-end"
+          }}>
+            <Button>Edit</Button>
+            <Button>Delete</Button>
+          </div>
+          <List>
+            <List.Item>
+              <b>Name:</b> {product.label}
+            </List.Item>
+            <List.Item>
+              <b>Type:</b> {product.type}
+            </List.Item>
+            <List.Item>
+              <b>Routine:</b>
+                <br />
+                <Tag color='pink'>AM</Tag>
+                <Tag color='cyan'>PM</Tag>
+              <div style={{display: 'block'}}>
+                {displayDaysOfWeek()}
+              </div>
+            </List.Item>
+          </List>
         </Layout>)}
     </Card>
   );
@@ -26,12 +84,16 @@ function ProductsPage() {
       routine: [
         {
           meridian: 'am',
-          dayOfWeek: 'Wednesday'
+          dayOfWeek: 'Monday'
         },
         {
           meridian: 'pm',
-          dayOfWeek: 'Friday'
+          dayOfWeek: 'Monday'
         },
+        {
+          meridian: 'pm',
+          dayOfWeek: 'Wednesday'
+        }
       ]
     },
     {
