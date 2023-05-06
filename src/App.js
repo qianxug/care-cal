0c8daae358550041509803d68b63a2b455624918
Leoy456 <leoyou2004@gmail.com>
2023-05-06 11:18:12 -0400

    MVP create account

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
+import {AuthProvider} from "./contexts/AuthContext"
 import DashboardPage from './components/DashboardPage';
 import ProductsPage from './components/ProductsPage';
 import NoPage from './components/NoPage';
+import RegisterPage from './components/RegisterPage';
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 };
---
+++ src/components/RegisterPage.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import { useState, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { Form, Input, Button, Card, Checkbox, Alert, Space } from 'antd';
+import { useAuth } from '../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom';
+
+const formItemLayout = {
+    labelCol: {
+      xs: {
+        span: 24,
+      },
+      sm: {
+        span: 8,
+      },
+    },
+    wrapperCol: {
+      xs: {
+        span: 24,
+      },
+      sm: {
+        span: 16,
+      },
+    },
+  };
+
+export default function RegisterPage() {
+    const navigate = useNavigate()
+    const { signup, currentUser } = useAuth()
+    const [loading, setLoading] = useState('')
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertType, setAlertType] = useState('');
+
+    const submitHandler = async (values) => {
+        console.log('Finish:', values);
+        console.log(values.name)
+
+        try {
+            setAlertMessage('')
+            setLoading(true)
+            await signup(values.email, values.password)
+            console.log("made user")
+            setAlertMessage("Account Created. Redirecting you to the login page.")
+            setAlertType('success')
+
+
+            navigate('/login')
+            
+        }
+        catch {
+            setAlertMessage('Failed to create user')
+            setAlertType('error')
+            console.log("failed")
+        }
+        setLoading(false)
+      };
+
+    return (
+        <>
+            <div style={{ backgroundColor: '#f2f2f2', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <Card title="Create an Account" bordered={true} style={{ width: 500 }}>
+                {alertMessage && (
+                    <Alert
+                    message={alertMessage}
+                    type={alertType}
+                    style = {{margin: 15}}
+                    showIcon
+                    closable
+                    onClose={() => {
+                        setAlertMessage('');
+                        setAlertType('');
+                    }}
+                    />
+                )}
+                    <Form
+                        {...formItemLayout}
+                        onFinish = {submitHandler}
+                    >
+                        <Form.Item
+                            name = "name"
+                            label = "Full Name"
+                            hasFeedback
+                            rules = {[
+                                {
+                                    required: true,
+                                    message: "Please enter your name"
+                                }
+
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            name = "email"
+                            label = "E-mail"
+                            rules = {[
+                                {
+                                    type: 'email',
+                                    message: 'The input is not a valid E-mail'
+                                },
+                                {
+                                    required: true,
+                                    message: "Please input a valid E-mail"
+                                }
+
+                            ]}
+                            hasFeedback
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            label="Password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                                {
+                                    min: 6,
+                                    message: 'Password must be at least 6 characters long',
+                                },
+                                {
+                                    max: 20,
+                                    message: 'Password cannot exceed 20 characters',
+                                },
+                            ]}
+                            hasFeedback
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item
+                            name="confirm password"
+                            label="Confirm Password"
+                            hasFeedback
+                            rules={[
+                            {
+                                required: true,
+                                message: 'Please re-enter your password!',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                                },
+                            })
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>   
+                        <Form.Item 
+                            name="care"
+                            valuePropName="checked"
+                            style = {{marginBottom: 0}}
+                        >
+                            <Checkbox>
+                            I intend to use care features
+                            </Checkbox>
+                        </Form.Item>    
+                        <Form.Item style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 0, marginTop: 0 }}>
+                            <Button type="primary" htmlType='submit' disabled={loading}>Create Account</Button>
+                        </Form.Item>
+                    </Form>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <p style={{ display: 'inline' }}>
+                        Already have an account? <a href="/login">Login</a>
+                        </p>
+                    </div>
+                </Card>
+                
+            </div>
+            
+        </>
+  )
+}
